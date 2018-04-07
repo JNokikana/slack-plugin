e06e6204c98ee5a840948c36c11bec205828b833
Joonas Nousiainen <herrapentti@gmail.com>
2018-04-07 14:17:46 +0300

    Some checks.

--- src/service/plugin.js
+++ src/service/plugin.js
@@ -1,15 +1,30 @@
 const SlackResponse = require('../util/SlackResponse.js');
 const Generator = require('../util/ResponseGenerator.js');
 
-class Plugin{
-    getPenis(request, reply){
+class Plugin {
+    getPenis(request, reply) {
+        let sliced;
+        let userId;
         let payload = request.payload;
         let command = payload.text;
-        console.log("Inc payload: ", payload);
-        let userId = command.slice(1, command.indexOf(">")).split("|")[0];
-        return new SlackResponse(Generator.generateResponse(userId), true);
+        let index = command.indexOf(">");
+
+        if (index > 1) {
+            sliced = command.slice(1, index);
+            userId = sliced.split("|");
+            if(sliced !== userId && userId.length === 2){
+                userId = userId[0];
+                return new SlackResponse(Generator.generateResponse(userId), true);
+            }
+            else{
+                return new SlackResponse("Invalid command", false);
+            }
+        }
+        else {
+            return new SlackResponse("Invalid command", false);
+        }
     }
-    getCredits(request, reply){
+    getCredits(request, reply) {
         let payload = request.payload;
         console.log("Inc payload: ", payload);
         return new SlackResponse("You can blame JNokikana for this atrocity", false);
