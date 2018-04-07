3c690ca9f5aa22daacfa52422dfbaf29e1a4d751
Joonas Nousiainen <herrapentti@gmail.com>
2018-04-07 13:17:17 +0300

    Port settings.

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
+require('dotenv').config({path: "./app.env"});
 const Hapi = require('hapi');
 const Server = new Hapi.Server({
-    port: process.env.PORT
+    port: process.env.PORT,
+    host: "localhost"
 });
 
 async function initServer() {
     await Server.register(require('./src/routes/app-routes.js'));
+    console.log(process.env.PORT);
     if (!module.parent) {
         await Server.start();
         console.log("Server started succesfully.");
